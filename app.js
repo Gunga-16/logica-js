842349243d48faecaf428d4285cc245faa245cc6
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-04 16:30:30 -0300

    numero_escolhido

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ alert('Boas vindas ao Jogo do Número Secreto');
 
 let numero_secreto = 13;
 
-prompt('Escolha um número entre 1 e 30');
+let numero_escolhido = prompt('Escolha um número entre 1 e 30');
+
+if (numero_escolhido == numero_secreto) {
+    prompt('Você acertou');
+} else {
+    prompt('Você errou');
+}
+
