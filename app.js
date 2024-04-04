3543060e9405d289724c878be66ed9105a10f8a1
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-04 16:31:15 -0300

    alert-numero_secreto

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let numero_secreto = 13;
 let numero_escolhido = prompt('Escolha um número entre 1 e 30');
 
 if (numero_escolhido == numero_secreto) {
-    prompt('Você acertou');
+    alert('Você acertou');
 } else {
-    prompt('Você errou');
+    alert('Você errou');
 }
 
