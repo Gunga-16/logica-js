2c148801fd0a16b2f8e7b3111d7ccc8ab5c12a3d
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-04 16:54:07 -0300

    botao-tentar_novamente

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 alert('Boas vindas ao Jogo do Número Secreto');
 
+const tentar_novamente = document.querySelector('.tentar_novamente');
+
 let min = 1;
 
 let max = 30;
@@ -8,18 +10,27 @@ const numero_secreto =  Math.floor(Math.random() * (max - min + 1)) + min;
 
 console.log('Número Secreto = ' + numero_secreto);
 
-let resposta = document.querySelector('.container__texto-azul');
+function sorteio(){
+    let numero_escolhido = prompt('Escolha um número entre 1 e 30');
 
-let descobriu = document.querySelector('.container__texto-azul_descobriu');
+    let resposta = document.querySelector('.container__texto-azul');
 
-let numero_escolhido = prompt('Escolha um número entre 1 e 30');
+    let descobriu = document.querySelector('.container__texto-azul_descobriu');
 
-if (numero_escolhido == numero_secreto) {
-    resposta.textContent = "acertou!";
-    descobriu.textContent = "descobriu";
 
-} else {
-    resposta.textContent = "não acertou!";
-    descobriu.textContent = "não descobriu";
+
+    if (numero_escolhido == numero_secreto) {
+        resposta.textContent = "acertou!";
+        descobriu.textContent = "descobriu";
+
+    } else {
+        resposta.textContent = "não acertou!";
+        descobriu.textContent = "não descobriu";
+    }
 }
 
+sorteio();
+
+if(tentar_novamente.onclick){
+    sorteio();
+}
